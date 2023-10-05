1b165cf9b8dbfb6465d4d818610254cc4c130648
Heliel Souza Lima <helielsouza01234@gmail.com>
2023-10-04 21:48:40 -0300

    Button access profile css and footer fixed

--- lab/models.py
+++ lab/models.py
@@ -1,11 +1,10 @@
-# from autoslug import AutoSlugField
+
 from django.db import models
 
 
 class Lab(models.Model):
     name = models.CharField(max_length=65)
     cpf = models.CharField(max_length=19)
-    # slug = AutoSlugField(unique=True, populate_from='name')
     created_at = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(
         upload_to='lab/reports/', blank=True, default='')
