56a6422298beccc14e03c8579fd8523e741a7e27
Heliel Souza Lima <helielsouza01234@gmail.com>
2023-11-12 23:44:17 -0300

    Admin fixed to comport new data from db

--- lab/admin.py
+++ lab/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Lab
+from .models import DetectedImage, Lab
 
 
 @admin.register(Lab)
@@ -8,4 +8,11 @@ class LabAdmin(admin.ModelAdmin):
     list_display = ['name', 'cpf', 'created_at']  # Campos exibidos na lista
     list_filter = ['created_at']  # Filtros disponíveis
     search_fields = ['name', 'cpf']  # Pesquisa por nome e CPF
+    list_per_page = 10
+
+
+@admin.register(DetectedImage)
+class DetectedAdmin(admin.ModelAdmin):
+    list_display = ['lab', 'detected_img', 'total_wbc', 'total_rbc',
+                    'total_plaquetas']
     list_per_page = 10
