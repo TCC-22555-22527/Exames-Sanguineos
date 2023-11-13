89c5435214fccb3e17099dac1f94321ac96e8cde
Heliel Souza Lima <helielsouza01234@gmail.com>
2023-11-13 16:22:00 -0300

    Adapting all theses changes at laudo_enviar func

--- lab/views.py
+++ lab/views.py
@@ -2,6 +2,7 @@ import os
 import re
 import shutil
 import subprocess
+from math import pi
 
 from authors.forms import AuthorReportForm, EditProfileForm
 from authors.models import Patient
@@ -33,7 +34,7 @@ def home(request):
     return render(request, 'lab/pages/home.html')
 
 
-# @has_permission_decorator('laudo_enviar_permission')
+@has_permission_decorator('laudo_enviar_permission')
 def laudo_enviar(request):
     form = AuthorReportForm()
     selected_patient = None
@@ -42,6 +43,10 @@ def laudo_enviar(request):
     total_rbc = 0
     total_wbc = 0
     total_plaquetas = 0
+
+    # conversao para micrometros 'um'
+    resolution_pixels_per_um = 25400
+    sizes_of_bounding_boxes_um = []
 
     if request.method == 'POST':
         # captura a imagem enviada no form
@@ -62,7 +67,6 @@ def laudo_enviar(request):
                 lab.save()
 
                 # Defina o caminho do arquivo de saída que vai ter rbc, etc.
-                output_file_path = "output.txt"
 
                 # Obtem o caminho da imagem que foi enviada e salva
                 image_path = os.path.join('media/' + lab.image.name)
@@ -71,54 +75,125 @@ def laudo_enviar(request):
                     "python yolov5/Inference_files/detect.py "
                     f"--source {image_path} "
                     "--weights yolov5/Inference_files/best_BCCM.pt "
-                    "--output lab_results/"
+                    "--output lab_results/ --save-txt"
                 )
 
                 # Execute o comando e redirecione a saída para o arquivo
-                with open(output_file_path, "w") as output_file:
-                    subprocess.run(detect_command, shell=True,
-                                   stdout=output_file)
+                subprocess.run(detect_command, shell=True)
+
+                txt_filename = os.path.splitext(
+                    os.path.basename(image_path))[0] + ".txt"
+                txt_filepath = os.path.join('lab_results', txt_filename)
+
+                with open(txt_filepath, "r") as txt_file:
+                    txt_content = txt_file.readlines()
+                with open(txt_filepath, "r") as txt_file:
+                    txt_content = txt_file.readlines()
+
+                # Imprima o conteúdo do arquivo de texto para análise
+                print("Conteúdo do arquivo de texto:")
+                print("".join(txt_content))
 
                 # Leia a saída do arquivo após o término do processo
-                with open(output_file_path, "r") as output_file:
-                    output_content = output_file.read()
-                    print(
-                        f"Output Content: {output_content}" + "fim da variável")  # noqa: E501
 
                 # Agora, você pode processar 'output_content' para extrair
-                matches = re.findall(r'(\d+)\s+(RBC|WBC|Platelets)', output_content)  # noqa: E501
+                matches = re.findall(
+                    r'(\d+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)', "".join(txt_content))  # noqa E501
                 print(f"Matches: {matches}")
 
                 if matches:
                     # Iterar sobre as correspondências
                     for match in matches:
-                        count, cell_type = match
+                        class_id, x_center, y_center, width, height = match
                         # Converter a contagem para um número
-                        count = int(count)
+                        print(f"\nClass ID: {class_id}\n")
 
-                        # Aqui, você pode adicionar a lógica para salvar os
-                        if cell_type == 'RBC':
-                            total_rbc += count
-                        elif cell_type == 'WBC':
-                            total_wbc += count
-                        elif cell_type == 'Platelets':
-                            total_plaquetas += count
+                        x_center = float(x_center)
+                        y_center = float(y_center)
+                        width = float(width)
+                        height = float(height)
 
-                    print(f"Total RBC: {total_rbc}, Total WBC: {total_wbc}, Total Platelets: {total_plaquetas}")  # noqa: E501
+                        # Converta para micrômetros usando a resolução
+                        x_center_um = x_center * resolution_pixels_per_um
+                        y_center_um = y_center * resolution_pixels_per_um
+                        width_um = width * resolution_pixels_per_um
+                        height_um = height * resolution_pixels_per_um
 
-                # move a imagem detectada do lab_results para outro lugar
+                        # área das bounding boxes
+                        area_um2 = width_um * height_um
+                        # salva na lista todas as áreas
+                        sizes_of_bounding_boxes_um.append(area_um2)
+                        average_size = sum(sizes_of_bounding_boxes_um) / len(
+                            sizes_of_bounding_boxes_um) if sizes_of_bounding_boxes_um else 0  # noqa E501
+
+                        if class_id == '0':
+                            total_plaquetas += 1
+                        elif class_id == '1':
+                            total_rbc += 1
+                        elif class_id == '2':
+                            total_wbc += 1
+
+                        print(
+                        f"\nTotal RBC: {total_rbc}, Total WBC: {total_wbc}, Total Platelets: {total_plaquetas}\n\n")  # noqa E501
+                        print(
+                            f"Coordenadas normalizadas (x, y, largura, altura): {x_center}, {y_center}, {width}, {height}")  # noqa E501
+                        print(
+                            f"Coordenadas em micrometros (x, y, largura, altura): {x_center_um}, {y_center_um}, {width_um}, {height_um}")   # noqa E501
+                        print(
+                            f"Área da bounding box em micrometros quadrados: {area_um2}, Tamanho médio em micrometros quadrados: {average_size}")  # noqa E501
+
+                        total_deteccoes = total_rbc + total_wbc + total_plaquetas  # noqa E501
+                        print(
+                            f"\nTotal de objetos detectados: {total_deteccoes}")  # noqa E501
+
+                        concentracao_rbc = round(
+                            (total_rbc / total_deteccoes) * 100, 2)
+                        concentracao_wbc = round(
+                            (total_wbc / total_deteccoes) * 100, 2)
+                        concentracao_plaquetas = round(
+                            (total_plaquetas / total_deteccoes) * 100, 2)
+                        concentracao_wbc_rbc = round(total_wbc / total_rbc, 4) if total_rbc != 0 else 0  # noqa E501
+                        media_diam_rbc = round(sum([float(match[3]) + float(match[4]) for match in matches if match[0] == '1']) / total_rbc * resolution_pixels_per_um, 2) if total_rbc != 0 else 0  # noqa E501
+                        media_diam_wbc = round(sum([float(match[3]) + float(match[4]) for match in matches if match[0] == '2']) / total_wbc * resolution_pixels_per_um, 2) if total_wbc != 0 else 0  # noqa E501
+                        media_circun_rbc = round(sum([(float(match[3]) + float(match[4])) / 4 * 2 * pi for match in matches if match[0] == '1']) / total_rbc * resolution_pixels_per_um, 2) if total_rbc != 0 else 0  # noqa E501
+                        media_circun_wbc = round(sum([(float(match[3]) + float(match[4])) / 4 * 2 * pi for match in matches if match[0] == '2']) / total_wbc * resolution_pixels_per_um, 2) if total_wbc != 0 else 0  # noqa E501
+
+                        # dividindo por mil para obter o valor em micrometros
+                        media_diam_rbc = round(media_diam_rbc / 1000, 2)
+                        media_diam_wbc = round(media_diam_wbc / 1000, 2)
+                        media_circun_rbc = round(media_circun_rbc / 1000, 2)
+                        media_circun_wbc = round(media_circun_wbc / 1000, 2)
+
+                        # impressoes no terminal
+                        print(
+                            f"\nConcentracao de RBC detectados: {concentracao_rbc} %")  # noqa E501
+                        print(
+                            f"\nConcentracao de WBC detectados: {concentracao_wbc} %")  # noqa E501
+                        print(
+                            f"\nConcentracao de Plaquetas detectadas: {concentracao_plaquetas} %")  # noqa E501
+                        print(
+                            f"\nConcentracao de WBC por RBC: {concentracao_wbc_rbc}")  # noqa E501
+                        print(
+                            f"\nDiametro medio RBC: {media_diam_rbc} um")
+                        print(
+                            f"\nDiametro medio WBC {media_diam_wbc} um")
+                        print(
+                            f"\nCircunferencia media RBC {media_circun_rbc} um")  # noqa E501
+                        print(
+                            f"\nCircunferencia media WBC {media_circun_wbc} um")  # noqa E501
 
                 detected_objects = os.listdir('lab_results/')
-                print(f"Detected objects: {detected_objects}")
+                print(f"\nDetected objects: {detected_objects}")
 
                 if detected_objects:  # Verifica se a lista não está vazia
-                    latest_detection = detected_objects[-1]
+                    latest_detection = next((f for f in detected_objects if f.lower().endswith(('.png', '.jpg', '.jpeg'))), None)  # noqa E501
+
                     source_path = os.path.join(
                         'lab_results', latest_detection)
                     destination_path = os.path.join(
                         'media/lab/detects', latest_detection)
                     shutil.move(source_path, destination_path)
-
+                    print(f"\nlatestest detecção: {latest_detection}")
                     # obtem a pk do imagem enviada
                     lab = Lab.objects.get(pk=lab.pk)
                     # salva os dados obtidos no bd da DetectedImage
@@ -127,7 +202,16 @@ def laudo_enviar(request):
                         detected_img=f'lab/detects/{latest_detection}',
                         total_wbc=total_wbc,
                         total_rbc=total_rbc,
-                        total_plaquetas=total_plaquetas)
+                        total_plaquetas=total_plaquetas,
+                        total_deteccoes=total_deteccoes,
+                        concentracao_rbc=concentracao_rbc,
+                        concentracao_wbc=concentracao_wbc,
+                        concentracao_plaquetas=concentracao_plaquetas,
+                        concentracao_wbc_rbc=concentracao_wbc_rbc,
+                        media_diam_rbc=media_diam_rbc,
+                        media_diam_wbc=media_diam_wbc,
+                        media_circun_rbc=media_circun_rbc,
+                        media_circun_wbc=media_circun_wbc)
                     detection_result.save()
 
                     messages.success(
@@ -135,8 +219,6 @@ def laudo_enviar(request):
                 else:
                     messages.error(
                         request, 'Nenhuma detecção encontrada em lab_results/.')  # noqa E501
-
-                # Limpeza: remova o arquivo de saída, se desejar
 
             except Patient.DoesNotExist:
                 messages.error(request, 'Paciente não encontrado.')
