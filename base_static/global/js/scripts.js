06c023000e847fe4c480e0991052c27d2b365232
Heliel Souza Lima <helielsouza01234@gmail.com>
2024-03-13 00:22:22 -0300

    Blurred effect after open menu

--- base_static/global/js/scripts.js
+++ base_static/global/js/scripts.js
@@ -15,38 +15,39 @@
 })();
 
 (() => {
-  const buttonCloseMenu = document.querySelector('.button-close-menu');
-  const buttonShowMenu = document.querySelector('.button-show-menu');
-  const menuContainer = document.querySelector('.menu-container');
-  const blurredContent = document.querySelector('.blurred-content'); // Elemento para aplicar o desfoque
+    const buttonCloseMenu = document.querySelector('.button-close-menu');
+    const buttonShowMenu = document.querySelector('.button-show-menu');
+    const menuContainer = document.querySelector('.menu-container');
+    const blurredContent = document.querySelector('.blurred-content-hidden'); // Elemento para aplicar o desfoque
 
+    const buttonShowMenuVisibleClass = 'button-show-menu-visible';
+    const menuHiddenClass = 'menu-hidden';
+    const blurredContentClass = 'blurred-content'; // Classe para aplicar o desfoque
 
-  const buttonShowMenuVisibleClass = 'button-show-menu-visible';
-  const menuHiddenClass = 'menu-hidden';
-  const blurredContentClass = 'blurred-content'; // Classe para aplicar o desfoque
+    const closeMenu = () => {
+        buttonShowMenu.classList.add(buttonShowMenuVisibleClass);
+        menuContainer.classList.add(menuHiddenClass);
+        blurredContent.classList.remove(blurredContentClass); // Remova a classe de desfoque ao fechar o menu
+    };
 
-  const closeMenu = () => {
-      buttonShowMenu.classList.add(buttonShowMenuVisibleClass);
-      menuContainer.classList.add(menuHiddenClass);
-      blurredContent.classList.remove(blurredContentClass); // Remova o desfoque ao fechar o menu
-  };
+    const showMenu = () => {
+        buttonShowMenu.classList.remove(buttonShowMenuVisibleClass);
+        menuContainer.classList.remove(menuHiddenClass);
+        blurredContent.classList.add(blurredContentClass); // Adicione a classe de desfoque ao abrir o menu
+    };
 
-  const showMenu = () => {
-      buttonShowMenu.classList.remove(buttonShowMenuVisibleClass);
-      menuContainer.classList.remove(menuHiddenClass);
-      blurredContent.classList.add(blurredContentClass); // Aplicar o desfoque ao abrir o menu
-  };
+    if (buttonCloseMenu) {
+        buttonCloseMenu.removeEventListener('click', closeMenu);
+        buttonCloseMenu.addEventListener('click', closeMenu);
+    }
 
-  if (buttonCloseMenu) {
-      buttonCloseMenu.removeEventListener('click', closeMenu);
-      buttonCloseMenu.addEventListener('click', closeMenu);
-  }
-
-  if (buttonShowMenu) {
-      buttonShowMenu.removeEventListener('click', showMenu);
-      buttonShowMenu.addEventListener('click', showMenu);
-  }
+    if (buttonShowMenu) {
+        buttonShowMenu.removeEventListener('click', showMenu);
+        buttonShowMenu.addEventListener('click', showMenu);
+    }
 })();
+
+
 
 (() => {
   const authorsLogoutLinks = document.querySelectorAll('.authors-logout-link');
